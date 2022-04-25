e9b50e82a598e4853ecbd6c3b5887cc972550013
JukemJ <93483466+JukemJ@users.noreply.github.com>
2022-04-25 11:19:08 -0400

    added card display

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ deck = ''
 fetch(url)
 .then(res => res.json()) // parse response as JSON
 .then(data => {
-  console.log(data)
+  //console.log(data)
   deck = data.deck_id
   console.log(deck)
 })
@@ -15,11 +15,19 @@ fetch(url)
 })
 
 function drawCard(){
-    fetch(`https://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`)
+    fetch(`https://deckofcardsapi.com/api/deck/${deck}/draw/?count=4`)
     .then(res => res.json()) 
     .then(data => {
-        playerHand.push(data.cards[0].code)
+        playerHand.push(data.cards[0].value)
+        playerHand.push(data.cards[1].value)
+        document.querySelector('#dealerCardOne').src = data.cards[0].image
+        document.querySelector('#dealerCardTwo').src = data.cards[1].image
+        document.querySelector('#remainingCards').innerText = data.remaining
+
+        dealerHand.push(data.cards[2].code)
+        dealerHand.push(data.cards[3].code)
         console.log(playerHand)
+        console.log(playerHand.reduce((a,b)=>Number(a) + Number(b)))
 
     })
     .catch(err => {
